a0e2771b514683af6d498d565be3ab243f88fe05
Jordan Vera <verawebdevelopment@gmail.com>
2018-11-17 04:31:41 -0600

    added particles js animation and media queries

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import '../../styles/css/Header.css';
 import { Container } from 'reactstrap';
 import Navigation from './Navbar/Navbar';
 import Particles from 'react-particles-js';
+import Fade from 'react-reveal/Fade';
 
 class Header extends Component {
   render() {
@@ -10,10 +11,10 @@ class Header extends Component {
       <div className='header'>
         <Navigation />
         <Container className='vertAlignCenter'>
+          <Fade>
             <h1>Full-stack Web Developer</h1>
-            <h3>
-            I design and code beautifully simple things, and I love what I do.
-            </h3>
+            <h3>I design and code beautifully simple things, and I love what I do.</h3>
+          </Fade>
         </Container>
         <Particles
           params={{
@@ -40,8 +41,12 @@ class Header extends Component {
                 }
               }
           }}
-          height="100vh"
-          width="100vw"  />
+          height="102%"
+          width="100vw"
+          style={{
+            "position": "absolute",
+            "top": "-2%"
+          }} />
       </div>
     );
   }
--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Col } from 'reactstrap';
 import Skills from '../Skills/Skills';
+import Bounce from 'react-reveal/Bounce';
 import '../../styles/css/Introduction.css';
 
 export default class Introduction extends Component {
@@ -8,10 +9,12 @@ export default class Introduction extends Component {
     return (
       <div className="introduction">
         <Container>
+          <Bounce left>
             <Col sm="12" md={{ size: 6, offset: 3 }}>
                 <h3>Hi, I'm Jordan.  Nice to meet you.</h3>
                 <p>Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually improving my chops.</p>
             </Col>
+          </Bounce>
         </Container>
         <Skills />
       </div>
