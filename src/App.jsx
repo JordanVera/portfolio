de2469384b3e6fde4cbfaec0c400ca65575141d6
Jordan Vera <verawebdevelopment@gmail.com>
2022-06-24 22:46:06 -0500

    added 404 page and react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import 'react-toastify/dist/ReactToastify.css';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import ButtonAppBar from '../Components/AppBar.js';
+import Jumbotron from '../Components/Jumbotron.js';
+import AboutMe from '../Components/AboutMe.js';
+import Skills from '../Components/Skills.js';
+import Projects from '../Components/Projects.js';
+import Footer from '../Components/Footer.js';
+import Resume from '../Components/Resume.js';
+import { ToastContainer } from 'react-toastify';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <ButtonAppBar />
+      <Jumbotron />
+      <Resume />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <ToastContainer
+        position="top-right"
+        theme="colored"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Footer />
+    </div>
+  );
+}
